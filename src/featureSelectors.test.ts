4697898cd1bd01279a8e296c67eb68eb691d2b0d
Garrett Kadillak <gkadillak@gmail.com>
2021-04-17 09:54:24 -0600

    test(featureselectors.test.ts): test the case where multiple entities are in the redux store

--- src/featureSelectors.test.ts
+++ src/featureSelectors.test.ts
@@ -5,19 +5,24 @@ import { makeGetDenormalizedData } from "./featureSelectors";
 describe("featureSelectors", () => {
   beforeAll(() => {
     const users = new schema.Entity("users");
+    const animals = new schema.Entity("animals");
     // @ts-expect-error
-    featureSliceImports.SCHEMA_MAP = { users };
+    featureSliceImports.SCHEMA_MAP = { users, animals };
   });
   describe("makeGetDenormalizedData", () => {
     it("should return the denormalized data", () => {
       const featureSlices = {
         entities: {
           users: { 1: { id: 1, name: "Garrett" }, 2: { id: 2, name: "Laura" } },
+          animals: { 1: { id: 1, name: "dog" }, 2: { id: 2, name: "cat" } },
         },
         users: {
           data: { users: [1, 2] },
-
           status: "blah",
+        },
+        animals: {
+          data: { animals: [1, 2] },
+          status: "foo",
         },
       };
       const getDenormalizedData = makeGetDenormalizedData({
